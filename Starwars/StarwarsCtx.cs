591f64da550697d9671515aa836153def2a33919
PatrickJessen <patrick-j1@hotmail.dk>
2023-03-20 10:44:27 +0100

    Add project files.

---
+++ Starwars/StarwarsCtx.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starwars
+{
+    public class StarwarsCtx : DbContext
+    {
+        public DbSet<Planet> Planet { get; set; }
+        public StarwarsCtx() : base()
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=PJJ-P15S-2022\\SQLEXPRESS;Database=myDataBase;Trusted_Connection=True;");
+        }
+    }
+}
